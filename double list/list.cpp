073ac3d04e738ab03889b2be6808c5a896a5da78
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-03-25 02:50:19 +0300

    Update list.cpp

--- double list/list.cpp
+++ double list/list.cpp
@@ -1,3 +1,6 @@
+#pragma GCC diagnostic ignored "-Wnon-template-friend"
+
+
 #include <iostream>
 #include <iterator>
 #include <vector>
@@ -9,8 +12,8 @@ namespace LIST {
 	class List {
 
 		private:
-			List<T> *         next;
-			List<T> *         prev;
+			List<T> * next;
+			List<T> * prev;
 			static uint64_t * head;
 			static uint64_t * tail;
 			static size_t     size; // size of List;
@@ -18,6 +21,7 @@ namespace LIST {
 
 		public:
 			void Connect(T m_elem, List<T> * prevElem = NULL);
+			void ConsistentShow();
 			List();
 			List(std:: vector<T> vect);
 			~List();
@@ -37,8 +41,6 @@ namespace LIST {
 
 int main(void) {
 
-	/*
-
 	LIST:: List<std:: string>   list1;
 	LIST:: List<std:: string>   list2;
 	LIST:: List<std:: string>   list3;
@@ -52,12 +54,30 @@ int main(void) {
 	std:: cout << std:: endl << std:: endl;
 	list3.Show();
 
+
+
+	/*
+
+	std:: vector<int> v(5);
+	int i = 0;
+	for(i = 0; i < 5; i++)
+		v[i] = i + 1;
+
+	LIST:: List<int> list1(v);
+	list1.ConsistentShow();
+
 	*/
 
 
 
-	std:: vector<int> v(5);
+	/*
 	
+	LIST:: List<int>   list1;
+	LIST:: List<int>   list2;
+	list1.Connect(1,   NULL);
+	list2.Connect(1, &list1);
+
+	*/
 
 	return 0;
 }
@@ -75,9 +95,23 @@ LIST:: List<T>:: List() {
 
 
 template<typename T>
-LIST:: List<T>:: List(std:: vector<T> v) {
+LIST:: List<T>:: List(std:: vector<T> vect) {
 
+	std:: vector<LIST:: List<T> > arrList(vect.size());
+	
+	int index = 0;
+	for(index = 0; index < vect.size(); index++) {
 
+		if (!index) {
+
+			arrList[index].Connect(vect[index], NULL);
+			continue;
+		}
+
+		arrList[index].Connect(vect[index], &arrList[index - 1]);
+	}
+
+	( * this) = arrList[0];
 }
 
 
@@ -88,17 +122,17 @@ void LIST:: List<T>:: Connect(T m_elem, List<T> * prevElem) {
 
 		tail = (uint64_t * )this;
 		head = (uint64_t * )tail;
-		next = NULL;
-		prev = prevElem;
+		next = (List<T> * )head;
+		prev = (List<T> * )prevElem;
 		size++;
 		elem = m_elem;
 
 		return;
 	}
 
-	tail = (uint64_t *)this;
-	next = NULL;
-	prev = prevElem;
+	tail = (uint64_t * )this;
+	next = (List<T> * )head;
+	prev = (List<T> * )prevElem;
 	prevElem->next = this;
 	size++;
 	elem = m_elem;
@@ -122,4 +156,17 @@ void LIST:: List<T>:: Show(void) const {
 	std:: cout << "current: " <<              this << std:: endl;
 	std:: cout << "size: "    <<              size << std:: endl;
 	std:: cout << "elem: "    <<              elem << std:: endl;
+	std:: cout <<                                     std:: endl;
+}
+
+
+template<typename T>
+void LIST:: List<T>:: ConsistentShow() {
+
+	LIST:: List<T> * save = this;
+	while(save) {
+
+		( * save).Show();
+		save = save->next;
+	}
 }
