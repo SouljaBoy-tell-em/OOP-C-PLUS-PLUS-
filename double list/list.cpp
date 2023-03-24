eff2b0143ec484222acb7d1aee55cee37e5a71fc
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-03-24 23:06:33 +0300

    Update list.cpp

--- double list/list.cpp
+++ double list/list.cpp
@@ -3,76 +3,107 @@
 #include <vector>
 
 
-template <typename T>
-class List {
+namespace LIST {
 
-	private:
-		List * next;
-		List * prev;
-		List * head;
-		List * tail;
-		size_t    size; // size of List;
+	static size_t size = 0;
 
-		friend 
+	template<typename T>
+	class List {
 
-	public:
-		List();
-		List(int num);
-		List(std:: vector<T> vect);
-		~List();
-		void Show(void) const;
-};
+		private:
+			List<T> * next;
+			List<T> * prev;
+			List<T> * tail;
+			size_t sizeList; // size of List;
+			T         elem;
+
+		public:
+			List();
+			List(T m_elem, List<T> * prevElem = NULL);
+			List(std:: vector<T> vect);
+			~List();
+			void Show(void) const;
+	};
+
+	uint64_t * head = NULL;
+}
 
 
 int main(void) {
 
 	std:: vector<int> v(5);
 
-	int i = 0;
-	for(i = 0; i < 5; i++)
-		v[i] = i + 1;
+	LIST:: List<int> list1(1,     NULL);
+	LIST:: List<int> list2(2,   &list1);
+	LIST:: List<int> list3(812, &list2);
+	list1.Show();
+	std:: cout << std:: endl << std:: endl;
+	list2.Show();
+	std:: cout << std:: endl << std:: endl;
+	list3.Show();
 
-	List<int> list(v);
- 
 
 	return 0;
 }
 
 
-template <typename T>
-List<T>:: List() {
+template<typename T>
+LIST:: List<T>:: List() {
 
-	next = prev = head = tail = NULL;
-	size = 0;
+	next = prev = tail = NULL;
+	size = 1;
+	sizeList = size;
+	elem = 0;
 }
 
 
-template <typename T>
-List<T>:: List(std:: vector<T> v) {
-
-
-}
-
-
-template <typename T>
-List<T>:: List(int num) {
+template<typename T>
+LIST:: List<T>:: List(std:: vector<T> v) {
 
 
 }
 
 
-template <typename T>
-List<T>:: ~List() {
+template<typename T>
+LIST:: List<T>:: List(T m_elem, List<T> * prevElem) {
+
+	if (!size) {
+
+		tail = this;
+		head = (uint64_t * )tail;
+		next = NULL;
+		prev = prevElem;
+		size++;
+		sizeList = size;
+		elem = m_elem;
+
+		return;
+	}
+
+	tail = this;
+	next =  NULL;
+	prev =  prevElem;
+	size++;
+	sizeList = size;
+	elem = m_elem;
+}
+
+
+template<typename T>
+LIST:: List<T>:: ~List() {
 
 	std:: cout << "closed." << std:: endl;
 }
 
-template <typename T>
-void List<T>:: Show(void) const {
+template<typename T>
+void LIST:: List<T>:: Show(void) const {
 
-	std:: cout << "INFO:"          << std:: endl;
-	std:: cout << "next: " << next << std:: endl;
-	std:: cout << "prev: " << prev << std:: endl;
-	std:: cout << "head: " << head << std:: endl;
-	std:: cout << "tail: " << size << std:: endl;
+	std:: cout << "INFO:"                          << std:: endl;
+	std:: cout << "next: "    <<              next << std:: endl;
+	std:: cout << "prev: "    <<              prev << std:: endl;
+	std:: cout << "head: "    << (uint64_t * )head << std:: endl;
+	std:: cout << "tail: "    <<              tail << std:: endl;
+	std:: cout << "current: " <<              this << std:: endl;
+	std:: cout << "size: "    <<              size << std:: endl;
+	std:: cout << "elem: "    <<              elem << std:: endl;
 }
